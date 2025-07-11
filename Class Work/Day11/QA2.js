9c0cf8a8da253c69cd4c5a99dda58116d1672c00
Aryan Sawant <aryansawant9012@gmail.com>
2025-07-11 10:23:14 +0530

    Qs2

---
+++ Class Work/Day11/QA2.js
@@ -0,0 +1,12 @@
+let name = 'Aryan';
+let len = name.length; 
+console.log(len)
+
+console.log(name[0])
+
+console.log(name[name.length - 1])
+
+console.log("apnacollege"+123)
+
+let str = " ";
+console.log(str.length)
