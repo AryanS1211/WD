0af42e6b030bb335e5ab7283fbe48d1e15965076
Aryan Sawant <aryansawant9012@gmail.com>
2025-07-11 11:19:33 +0530

    Qs

---
+++ Class Work/Day11/QA4.js
@@ -0,0 +1,13 @@
+let pop_size = 'XL';
+if (pop_size == "XL") {
+  console.log(`Prize of Popcorn is 250/- for ${pop_size}`);
+} else if (pop_size == "L") {
+  console.log(`Prize of Popcorn is 200/- for ${pop_size}`);
+} else if (pop_size == "M") {
+  console.log(`Prize of Popcorn is 100/- for ${pop_size}`);
+} else if (pop_size == "S") {
+  console.log(`Prize of Popcorn is 50/- for ${pop_size}`);
+}
+else{
+    console.log("Enter Correct Input")
+}
