9182b22ed07707aca82947a857244349f59abaa1
Aryan Sawant <aryansawant9012@gmail.com>
2025-07-12 13:19:48 +0530

    Day12

---
+++ Class Work/Day12/QS1.js
@@ -0,0 +1,7 @@
+let word = "help!";
+let newword = word.trim().toUpperCase();
+console.log(newword);
+
+let name = 'ApnaCollege';
+let upname = name.slice(4, name.length).replace("l", "t").replace("l", "t");
+console.log(upname);
