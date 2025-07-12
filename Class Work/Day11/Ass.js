adc7be7a155ebb4bf56a91c03d3325e60ce5bcea
Aryan Sawant <aryansawant9012@gmail.com>
2025-07-12 12:33:46 +0530

    Ass

---
+++ Class Work/Day11/Ass.js
@@ -0,0 +1,77 @@
+// QS 2
+
+// let name = prompt("Enter Name :");
+// undefined;
+// let age = prompt("Enter age :");
+// undefined;
+// alert(`${name} is ${age} years old`);
+
+// QS1
+
+// let num = 33;
+// if (num%10 == 0){
+//     console.log("Number is good");
+// }
+// else{
+//     console.log("Number is bad");
+// }
+
+// QS3
+
+// let quater = 1;
+// switch (quater) {
+//   case 1:
+//     console.log("Months in quater 1 are : Jan , Feb , March");
+//     break;
+//   case 2:
+//     console.log("Months in quater 1 are : Apirl , May , June");
+//     break;
+//   case 3:
+//     console.log("Months in quater 1 are : July , August , Sep");
+//     break;
+//   case 4:
+//     console.log("Months in quater 1 are : Oct , Nov , Dec");
+//     break;
+//   default:
+//     console.log("Enter correct quater");  
+// }
+
+// //QS 4
+
+// let str = "dfnvowsle";
+// let len = str.length;
+// if (len>5 && str[0] == 'a'){
+//     console.log("The string is golden string");
+// }
+// else{
+//     console.log("Not a golden string");
+// }
+
+// //QS 5
+
+// let num1 = 4;
+// let num2 = 6; 
+// let num3 = 99;
+// if ((num1 > num2) && (num1 > num3)){
+//     console.log("Num1 is greater number");
+// }
+// else if((num2> num1) && (num2 > num3)){
+//     console.log("Num2 is greater number");
+// }
+// else{
+//     console.log("Num3 is greater number");
+// }
+
+// //QS6
+
+// let num1 = 32888;
+// let num2 = 44322;
+
+// let cnum1 = num1%10;
+// let cnum2 = num2%10;
+// if ((cnum1 && cnum2) == 2){
+//     console.log("Both have same last digit");
+// }
+// else{
+//     console.log("Both don't have same last digit");
+// }
