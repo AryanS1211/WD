adca15cd66ce61d211a90b86e59056fb574cae0a
Aryan Sawant <aryansawant9012@gmail.com>
2025-07-12 15:55:28 +0530

    Qs

--- Class Work/Day12/Qs2.js
+++ Class Work/Day12/Qs2.js
@@ -1,7 +1,21 @@
-let arr = ['jan','july','march','august']
+// let arr = ['jan','july','march','august']
 
-arr.shift()
-arr.shift()
-arr.unshift('june')
-arr.unshift("july");
-console.log(arr)
+// arr.shift()
+// arr.shift()
+// arr.unshift('june')
+// arr.unshift("july");
+// console.log(arr)
+
+//QS1
+
+let start = ["jan", "july", "march", "august"];
+
+console.log(start.splice(0,2,'july','june'))
+
+console.log(start)
+
+//QS2
+
+let arr = ["C",'c++','html','js','py','java','c#','sql'];
+
+console.log(arr.reverse().indexOf('js'))
