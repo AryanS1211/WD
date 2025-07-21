c9a5c3dac5255840b93c83ff917cb7b1231ed8a6
Aryan Sawant <aryansawant9012@gmail.com>
2025-07-21 16:04:47 +0530

    QS-Day15

---
+++ Class Work/Day15/QS.js
@@ -0,0 +1,24 @@
+//QS 1
+
+let a = Math.floor(Math.random() * 6) + 1;
+console.log(a);
+
+//QS2
+
+let car = {
+  name: "BMW",
+  model: "X5",
+  color: "Black",
+};
+console.log(car);
+
+//QS3
+
+let person = {
+  name1: "Aryan",
+  age: 20,
+  city: "Pune",
+};
+person.city = "New york";
+person.country = "USA";
+console.log(person);
